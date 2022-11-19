54e1523c85adcdc5783973cf98bf279a0aa5ed1b
lsx <1468709106@qq.com>
2022-11-19 19:37:51 +0800

    demo

---
+++ hello-world-demo/node-v8/build/Release/.deps/Release/obj.target/hello/hello.o.d
@@ -0,0 +1,19 @@
+cmd_Release/obj.target/hello/hello.o := c++ -o Release/obj.target/hello/hello.o ../hello.cc '-DNODE_GYP_MODULE_NAME=hello' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node -I/Users/shuxinlin/Library/Caches/node-gyp/16.18.0/src -I/Users/shuxinlin/Library/Caches/node-gyp/16.18.0/deps/openssl/config -I/Users/shuxinlin/Library/Caches/node-gyp/16.18.0/deps/openssl/openssl/include -I/Users/shuxinlin/Library/Caches/node-gyp/16.18.0/deps/uv/include -I/Users/shuxinlin/Library/Caches/node-gyp/16.18.0/deps/zlib -I/Users/shuxinlin/Library/Caches/node-gyp/16.18.0/deps/v8/include  -O3 -gdwarf-2 -mmacosx-version-min=10.13 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++14 -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/hello/hello.o.d.raw   -c
+Release/obj.target/hello/hello.o: ../hello.cc \
+  /Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/node.h \
+  /Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/v8.h \
+  /Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/cppgc/common.h \
+  /Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/v8config.h \
+  /Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/v8-internal.h \
+  /Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/v8-version.h \
+  /Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/v8-platform.h \
+  /Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/node_version.h
+../hello.cc:
+/Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/node.h:
+/Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/v8.h:
+/Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/cppgc/common.h:
+/Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/v8config.h:
+/Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/v8-internal.h:
+/Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/v8-version.h:
+/Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/v8-platform.h:
+/Users/shuxinlin/Library/Caches/node-gyp/16.18.0/include/node/node_version.h:
