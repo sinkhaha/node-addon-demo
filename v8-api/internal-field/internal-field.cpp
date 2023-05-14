27c56e8f8ccff9e03aa5a963887d3003967ca59a
lsx <1468709106@qq.com>
2023-05-14 22:39:35 +0800

    fix

--- v8-api/internal-field/internal-field.cpp
+++ v8-api/internal-field/internal-field.cpp
@@ -19,49 +19,54 @@ using v8::Array;
 using v8::MaybeLocal;
 using v8::Context;
 
-// 自定义数据结构
+// 自定义数据结构，内部加了一个persistent持久句柄，作用是用于回收自定义字段
 struct PersistentWrapper {
     Persistent<Object> persistent; // 持久句柄
     int value;
 };
 
+/**
+ * 弱持久句柄的回调函数
+ * 当对象被回收时，此回调会被触发
+ */
 void WeakCallback(const v8::WeakCallbackInfo<PersistentWrapper>& data)
 {
     PersistentWrapper* wrapper = data.GetParameter(); // 获取到SetWeak方法传的第一个参数，此时是wrapper参数
 
-    printf("deleting 0x%.8X: %d...", wrapper, wrapper->value); // NOLINT
+    printf("deleting 0x%.8X: %d...", wrapper, wrapper->value);
 
-    wrapper->persistent.Reset(); // 重置这个持久句柄
-    delete wrapper; // 删除堆内存分配的指针
+    wrapper->persistent.Reset(); // 重置(释放)这个持久句柄
+
+    delete wrapper; // 删除wrapper对象，即删除堆内存分配的指针
 
     printf("ok\n");
 }
 
-// 当obj对象被垃圾回收时，会触发一个WeakCallback回调，这个回调里主要是删除new出来的PersistentWrapper对象
+/**
+ * 当obj对象被垃圾回收时，会触发WeakCallback回调，这个回调里主要是删除new出来的PersistentWrapper对象
+ */
 void CreateObject(const FunctionCallbackInfo<Value>& args)
 {
     Isolate* isolate = args.GetIsolate();
     Local<Context> context = isolate->GetCurrentContext();
 
-    // 新建对象模板
     Local<ObjectTemplate> templ = ObjectTemplate::New(isolate);
+    // 设置对象模板的内置字段数为1
     templ->SetInternalFieldCount(1);
 
-    // 新建对象以及设置内置字段
+    // 实例子化PersistentWrapper
     PersistentWrapper* wrapper = new PersistentWrapper();
-    // 方法传入的第一个参数赋值给valu
+    // 方法传入的第一个参数赋值给value
     wrapper->value = args[0]->ToNumber(context).ToLocalChecked()->Int32Value(context).FromJust();
     
-    // obj模板对象设置内置字段
+    // obj模板对象设置内置字段，值为wrapper对象
     Local<Object> obj = templ->NewInstance(context).ToLocalChecked();
-    obj->SetInternalField(0, External::New(isolate, wrapper));
+    obj->SetInternalField(0, External::New(isolate, wrapper)); // 时内置字段是一个 External 永生句柄类型
 
-    // 基于 obj 新建持久句柄
-    wrapper->persistent.Reset(isolate, obj); // 重置这个持久句柄
-
-    // 将持久句柄设置为弱持久句柄。wrapper参数可以在WeakCallback方法中获取到
+    // 重置(释放)wrapper的持久句柄
+    wrapper->persistent.Reset(isolate, obj);
+    // 将持久句柄设置为弱持久句柄，回调函数为WeakCallback（当被回收时被调用）。wrapper参数可以在WeakCallback方法中获取到
     wrapper->persistent.SetWeak(wrapper, WeakCallback, v8::WeakCallbackType::kInternalFields);
-    // wrapper->persistent.MarkIndependent(); // TODO 标记独立句柄 此版本没有此函数
 
     args.GetReturnValue().Set(obj);
 }
@@ -73,7 +78,7 @@ void Init(Local<Object> exports, Local<Object> module)
     HandleScope scope(isolate);
 
     // 暴露create函数，其函数体为CreateObject
-    exports->Set(context, String::NewFromUtf8(isolate, "create").ToLocalChecked(), FunctionTemplate::New(isolate, CreateObject)->GetFunction(context).ToLocalChecked());
+    exports->Set(context, String::NewFromUtf8(isolate, "create").ToLocalChecked(), FunctionTemplate::New(isolate, CreateObject)->GetFunction(context).ToLocalChecked()).Check();
 }
 
 NODE_MODULE(_template, Init)
--- v8-api/internal-field/test.js
+++ v8-api/internal-field/test.js
@@ -1,7 +1,22 @@
 const internalField = require('./build/Release/internal-field');
 
-// 创建10个对象，这些对象都与各自的WeakCallback关联起来
-for (let i = 0; i < 10; i++) {
-    const rst = internalField.create(i);
-    console.log(rst); 
+function test() {
+    const result = {};
+    // 创建3个对象，这些对象都与各自的WeakCallback关联起来
+    for (let i = 0; i < 3; i++) {
+        result[i] = internalField.create(i);
+    }
+    return result;
 }
+
+const result = test();
+
+console.log(result); // { '0': {}, '1': {}, '2': {} }
+
+// 删除每个对象，运气好的话，V8的垃圾回收机制会触发
+delete result[0];
+delete result[1];
+delete result[2];
+
+console.log(result); // {}
+
